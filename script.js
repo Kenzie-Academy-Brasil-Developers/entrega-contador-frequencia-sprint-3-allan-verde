91deed9bc538ddeefa582b848e1756af8c9e9bc0
Allan Verde <allangreen2906@gmail.com>
2021-09-08 22:55:54 -0300

    Ajustando variáveis

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//let typedText = document.getElementById("textInput").value
 //let typedText = "One Fish, Two Fish, Red Fish, Blue Fish By Dr. Seus One fish Two fish Red fish Blue fish. Black fish Blue fish Old fish New fish. This one has a little star. This one has a little car. Say! What a lot Of fish there are. Yes. Some are red. And some are blue. Some are old. And some are new. Some are sad. And some are very, very bad. Why are they Sad and glad bad? I do not know. Go ask your dad. Some are thin. And some are fato. The fat one has A yellow hat. From there to here, from here to there, Funny things Are everywhere. Here are some Who like to run. They run for fun In the hot, hot sun. "
-let typedText = ' One Fish'
+let typedText = document.getElementById("textInput").value
 typedText = typedText.toLowerCase(); 
 typedText = typedText.replace(/[^a-z'\s]+/g, ""); 
 
