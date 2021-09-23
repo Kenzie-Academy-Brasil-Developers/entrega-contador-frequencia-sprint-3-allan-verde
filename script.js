762dd1e713b966ce29957bb88574d5c36fa443d8
Allan Verde <allangreen2906@gmail.com>
2021-09-23 12:33:52 -0300

    Adicionando contador de palavras e estilizaçao

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const button = document.getElementById("countButton");
 
 button.addEventListener("click", function() {  
+    const words = document.getElementById('wordsDiv')
+    const letters = document.getElementById("lettersDiv")
+    // NECESSÁRIO PARA QUANDO CLICAR NOVAMENTE NO BOTÃO LIMPAR O QUE TINHA SIDO FEITO ANTES
+    words.innerText = ''
+    letters.innerText = ''
+
     let typedText = document.getElementById("textInput").value
     typedText = typedText.toLowerCase(); 
     typedText = typedText.replace(/[^a-z'\s]+/g, ""); 
@@ -19,8 +25,25 @@ button.addEventListener("click", function() {
         const span = document.createElement("span"); 
         const textContent = `"${letter}": ${letterCounts[letter]}, `;
         span.innerText = textContent; 
-        const letters = document.getElementById("lettersDiv");
         letters.appendChild(span); 
+    }
+    //-------------------------------------------------
+    const wordCounts = {}
+    let arrTypedText = typedText.split(' ')
+    for (let i = 0; i < arrTypedText.length; i++) {
+        let currentWord = arrTypedText[i]
+
+        if (wordCounts[currentWord] === undefined) {
+            wordCounts[currentWord] = 1
+        } else {
+            wordCounts[currentWord]++
+        }
+    }
+    for (let word in wordCounts) {
+        const span = document.createElement('span')
+        const textContent = `${word}: ${wordCounts[word]}, `
+        span.innerText = textContent
+        words.appendChild(span)
     }
 });
 //words = typedText.split(/\s/);
